c1a1fd4807ad3d62647160049a3e39e32982f2bd
areekaras <shibili.areekara@gmail.com>
2026-02-12 06:53:57 +0800

    Initial commit with template project

---
+++ Template/UnifiedContacts/UnifiedContacts/Contact.swift
@@ -0,0 +1,14 @@
+//
+//  Contact.swift
+//  UnifiedContacts
+//
+//  Created by Shibili Areekara on 12/02/26.
+//
+
+import Foundation
+
+struct Contact {
+    let firstName: String
+    let lastName: String
+    let emailAddress: String
+}
---
+++ Template/UnifiedContacts/UnifiedContactsTests/UnifiedContactsTests.swift
@@ -0,0 +1,16 @@
+//
+//  UnifiedContactsTests.swift
+//  UnifiedContactsTests
+//
+//  Created by Shibili Areekara on 12/02/26.
+//
+
+import Testing
+
+struct ContactListViewModelTests {
+    
+    @Test
+    func assertContactList() async throws {
+        
+    }
+}
