45ed322e4a97aea58e37c2a203415ae547eaaa00
areekaras <shibili.areekara@gmail.com>
2026-02-12 07:39:01 +0800

    test: refactor test suite to conform to XCTestCase

---
+++ Lead-Solution/UnifiedContacts/UnifiedContactsTests/ContactListViewModelTests.swift
@@ -0,0 +1,12 @@
+//
+//  ContactListViewModelTests.swift
+//  UnifiedContactsTests
+//
+//  Created by Shibili Areekara on 12/02/26.
+//
+
+import XCTest
+
+final class ContactListViewModelTests: XCTestCase {
+    
+}
